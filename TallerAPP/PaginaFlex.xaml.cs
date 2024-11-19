8b6d60b000a9f431aa1d84eaca9cf691af15ed06
José Miguel Vásconez Andrade <jose.vasconez.andrade@udla.edu.ec>
2024-11-19 12:09:02 -0500

    Se agrego las imagenes y la funcion de darle clic a cualquier imagen se va a la pagina Absolute.

--- TallerAPP/PaginaFlex.xaml.cs
+++ TallerAPP/PaginaFlex.xaml.cs
@@ -6,4 +6,9 @@ public partial class PaginaFlex : ContentPage
     {
         InitializeComponent();
     }
+
+    private void Imagen_Click(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new PaginaAbsolut());
+    }
 }
