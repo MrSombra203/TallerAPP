bf607f635935d100d81447526cf4af724245511c
José Miguel Vásconez Andrade <jose.vasconez.andrade@udla.edu.ec>
2024-11-19 13:13:51 -0500

    Se agrego todas las funcionalidades de absolut y al hacer click en el boton se va de regreso al grid .

--- TallerAPP/PaginaAbsolut.xaml.cs
+++ TallerAPP/PaginaAbsolut.xaml.cs
@@ -6,4 +6,9 @@ public partial class PaginaAbsolut : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Botion4_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new PaginaGrid());
+    }
 }
